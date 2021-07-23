05e505166b9977e26cebf7a3f84fa60dbbc1b8d4
Piotr Szczepanik <piter75@gmail.com>
2021-07-23 22:34:29 +0200

    Added not affected example

--- src/main/scala/example/MyProcessFunction.scala
+++ src/main/scala/example/MyProcessFunction.scala
@@ -1,5 +1,8 @@
 package example
 
+import avro.shaded.com.google.common.collect.BiMap
 import org.apache.beam.sdk.transforms.ProcessFunction
 
 trait MyProcessFunction[T, V] extends ProcessFunction[T, V]
+
+trait MyBiMap[K, V] extends BiMap[K, V]
